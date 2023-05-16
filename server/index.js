0a839e57c70235cb328a948c08a9af265d2e09a2
phamtrongan2710 <phamtrongan2710@gmail.com>
2023-05-16 22:28:07 +0700

    create db

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const oracledb = require('oracledb');
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+
+async function fun() {
+  let con;
+
+  try {
+    con = await oracledb.getConnection({
+      user: "sys",
+      password: "123456",
+      connectionString: "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVER=G07)(SID=xe)))",
+      privilege: oracledb.SYSDBA
+    })
+
+    const data = await con.execute(
+      'SELECT * FROM TB_ACCOUNT',
+    )
+    console.log(data.rows);
+    
+  } catch(err) {
+    console.error(err);
+  }
+}
+fun();
