1c9b55e456907f6452cc4f752fe0ec05176aa474
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-06-04 00:22:06 +0700

    signin & signup pages

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,9 +1,13 @@
 import Home from "../pages/Home/Home";
 import Products from "../pages/Products";
+import Signin from "../pages/Signin";
+import Signup from "../pages/Signup";
 
 const publicRoutes = [
     { path: "/", component: Home },
     { path: "/product", component: Products },
+    { path: "/signin", component: Signin },
+    { path: "/signup", component: Signup },
 ];
 
 export { publicRoutes };
