b9d4b11d6fb08f6498634b1d7bf1af0f13e100ff
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-06-08 13:02:33 +0700

    retouch cart items ui

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,23 +1,53 @@
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+// icons
+import { HiMinusSm } from "react-icons/hi";
+import { IoMdAdd } from "react-icons/io";
+
 const CartItem = ({ data }) => {
     return (
         <div className="flex items-center mb-2">
             {/* product image */}
-            <img className="w-24" src={data.data.data.image[0]} />
+            <Link to={`/product/${data.data.data.id}`} state={data.data.data}>
+                <img className="w-24" src={data.data.data.image[0]} />
+            </Link>
 
             <div className="pl-4">
                 {/* product name */}
-                <p className="font-medium hover:underline">
-                    {data.data.data.name}
-                </p>
+                <Link
+                    to={`/product/${data.data.data.id}`}
+                    state={data.data.data}
+                >
+                    <p className="font-medium hover:underline">
+                        {data.data.data.name}
+                    </p>
+                </Link>
 
                 {/* product price */}
                 <p className="">$ {data.data.data.price}.00</p>
 
-                {/* amount */}
-                <p className="w-4">Amount: {data.data.amount}</p>
+                <div className="flex items-center mt-2.5">
+                    {/* quantity */}
+                    <div className="flex items-center overflow-hidden rounded bg-slate-200">
+                        {/* "-" icon */}
+                        <button className="p-2">
+                            <HiMinusSm />
+                        </button>
 
-                {/* remove button */}
-                <button className="opacity-80 text-sm underline">Remove</button>
+                        {/* amount */}
+                        <p className="w-4">{data.data.amount}</p>
+
+                        {/* "+" icon */}
+                        <button className="p-2">
+                            <IoMdAdd />
+                        </button>
+                    </div>
+
+                    {/* remove button */}
+                    <button className="opacity-80 text-sm underline p-2 ml-2">
+                        Remove
+                    </button>
+                </div>
             </div>
         </div>
     );
